c55ff7b31dc248724ab4b155a743f368271b182d
Matheus Araripe <matheus.alc2001@gmail.com>
2022-10-24 22:20:39 -0300

    final code

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get('/news', (req, res) => {
     res.json(articles)
 })
 
-app.get('/news/:newspaperId', async (req, res) => {
+app.get('/news/:newspaperId', (req, res) => {
     const newspaperId = req.params.newspaperId
 
     const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
